f5ffa990749228b3126b62dc726a3146031fbb38
Adriel Garcia <adrielgarcia@live.com>
2022-10-12 15:36:20 -0300

    chore: setup font family

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.tsx',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: [
+		'./src/**/*.tsx',
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: 'Inter, sans-serif',
+			},
+		},
+	},
+	plugins: [],
+};
